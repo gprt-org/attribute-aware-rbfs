3e2c65772f40deaa5d2c50a62ef0d7b1395c07c0
Stefan Zellmann <info@szellmann.de>
2023-03-24 12:46:46 +0100

    injection_d50 benchmark

---
+++ scripts/benchmark/injection_d50_Tin0.95.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# don't change:
+name="injection_d50_Tin0.95"
+
+#
+# USER SET:
+#
+bindir=/home/szellmann/point-clouds/build
+outdir=/home/szellmann/point-clouds-benchmarks/${name}
+data="--mmpld /home/szellmann/Downloads/injection_d50_Tin0.95_Tout1.05_vfeed0.002.tiff"
+orbit_count="--orbit 50"
+orbit_center="--orbit-center 100 197.501 100"
+orbit_up="--orbit-up 0 -1 0"
+orbit_radius="--orbit-radius 400"
+
+##
+scriptdir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "Create if not exists (outdir): "${outdir}
+mkdir -p ${outdir}
+cp ${scriptdir}/${name}.ini ${outdir}/viewer.ini
+cd ${outdir}
+echo "Running benchmark in: "$PWD
+${bindir}/viewer-headless ${data} ${orbit_count} ${orbit_center} ${orbit_up} ${orbit_radius} --benchmark
